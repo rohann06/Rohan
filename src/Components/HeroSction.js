614c065693dcb92fb4c20f2f93c4759c29b1a23f
Rohan <rohanahire651@gmail.com>
2022-07-09 16:25:51 +0530

    first commit

---
+++ src/Components/HeroSction.js
@@ -0,0 +1,51 @@
+import "./HeroSection.css"
+//Images
+import img from "../images/my_img.png";
+import tweeter from "../images/tweeter.png";
+import github from "../images/github.png";
+import linkedin from "../images/linkedin.png"
+
+
+const Hrosecton = () => {
+    return ( 
+        <div className="hero-section">
+          <div className="deceription">
+            <h1>
+              Hi!
+              <img
+                src="https://raw.githubusercontent.com/nixin72/nixin72/master/wave.gif"
+                alt="gif"
+                height="35"
+                width="35"
+              />
+              I'm <br/> <h1 id="name">Rohan</h1>
+            </h1>
+
+            <p>
+              An inspiring full-stack web3 dev...🔗🚀 </p>
+              <p>who used to be a data analyst.📈
+            </p>
+          </div>
+
+          <div className="images">
+            <img id="My_img" src={img} alt="my_img" />
+            <div className="contacts">
+
+              <a  href="https://twitter.com/Rohan651">
+                <img id="tw" src={tweeter} alt="" />
+              </a>
+
+              <a href="https://github.com/rohanA6">
+                <img id="gh" src={github} alt="" />
+              </a>
+
+              <a href="https://www.linkedin.com/in/rohan-ahire-652001/">
+                <img id="in" src={linkedin} alt="" />
+              </a>
+            </div>
+          </div>
+        </div>
+     );
+}
+ 
+export default Hrosecton;
---
+++ src/Components/MySkills.js
@@ -0,0 +1,24 @@
+import "./MySkills.css";
+
+const MySkills = () => {
+  return (
+    <div className="MySkills">
+      <div className="skills">
+        <h2>What I'm offering</h2>
+        <p>
+          I'm an inspiring full-stack develope and <a href="https://twitter.com/developer_dao">Developer DAO </a> who can do a good work
+          with ....
+          <h2 className="skills-items">
+            <ul>
+            <li>solidity</li> 
+            <li>javaScript</li>
+            <li>reactJs/NextJS</li>
+            </ul>
+          </h2>
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default MySkills;
